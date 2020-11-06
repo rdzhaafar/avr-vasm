f9fbfc05d0318672cc4c133e397e4dc6c53bf31e
Rida Dzhaafar <rdzhaafar@gmail.com>
2020-11-06 17:18:28 -0500

    simplified opcodes table

--- cpus/avr/cpu.h
+++ cpus/avr/cpu.h
@@ -5,13 +5,16 @@
 
 #define BIGENDIAN 0
 #define LITTLEENDIAN 1
+
 #define VASM_CPU_AVR 1
+
 #define MAX_OPERANDS 2
+
 #define MAX_QUALIFIERS 0
+
 #define INST_ALIGN 1
 #define DATA_ALIGN(n) 1
-#define DATA_OPERAND(n) DATA8
-#define MAX_QUALIFIERS 0
+#define DATA_OPERAND(n) IMM
 
 #define PARSE_CPU_LABEL(l, s) 0
 #define MNEMONIC_VALID(idx) 1
@@ -19,39 +22,28 @@
 typedef int32_t taddr;
 typedef uint32_t utaddr;
 
-/* operand types */
-enum op_t {
+/* avr operand types */
+typedef enum avr_op_t {
   DEFAULT = 0,
-  NONE,         /* implied operand                      */
-  REG_ALL,      /* all registers  R[0-31]               */
-  REG_HI,       /* high registers R[16-31]              */
-  REG_FMUL,     /* fmul* instruction registers R[16-23] */
-  WREG_ALL,     /* all "word" registers R[0,2,4...30]   */
-  WREG_HI,      /* high "word" registers R[24,26,28,30] */
-  IOREG_ALL,    /* all i/o registers [0-63]             */
-  IOREG_LO,     /* low i/o registers [0-31]             */
-  ADDR,         /* SRAM address                         */
-  X,            /* X-register                           */
-  X_PL,         /* X+                                   */
-  MI_X,         /* -X                                   */
-  Y,            /* Y-register                           */
-  Y_PL,         /* Y+                                   */
-  MI_Y,         /* -Y                                   */
-  Y_Q,          /* Y+q                                  */
-  Z,            /* Z-register                           */
-  Z_PL,         /* Z+                                   */
-  MI_Z,         /* -Z                                   */
-  Z_Q,          /* Z+q                                  */
-  BIT_N,        /* bit number [0-7]                     */
-  DATA8,        /* immediate data 8-bit                 */
-  DATA7,        /* immedaite data 7-bit                 */
-  DATA6,        /* immediate data 6-bit                 */
-  BRA,          /* relative branch offset [(-64)-63]    */
-  DES,          /* number of DES iterations [0-16]      */
+  NONE,        /* No operand     */
+  REG,         /* Register       */
+  REGR,        /* Register range */
+  X,           /* X-register     */
+  X_PLUS,      /* X+             */
+  MINUS_X,     /* -X             */
+  Y,           /* Y-register     */
+  Y_PLUS,      /* Y+             */
+  MINUS_Y,     /* -Y             */
+  Y_PLUS_Q,    /* Y+q            */
+  Z,           /* Z-register     */
+  Z_PLUS,      /* Z+             */
+  MINUS_Z,     /* -Z             */
+  Z_PLUS_Q,    /* Z+q            */
+  IMM,         /* Immediate      */
 };
 
 typedef struct {
-  enum op_t type;
+  avr_op_t type;
   uint16_t value;
 } operand;
 
