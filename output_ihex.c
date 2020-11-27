fa9ff24b7b29e85db2441df32c30f57126881973
Rida Dzhaafar <rdzhaafar@gmail.com>
2020-11-27 10:54:35 -0500

    ihex now supports 00, 01, 02 and 04 records

--- output_ihex.c
+++ output_ihex.c
@@ -2,107 +2,112 @@
 
 #ifdef OUTIHEX
 
-#define I8HEX  0
-#define I16HEX 1
-#define I32HEX 2
+/* ihex formats */
+#define I8HEX  0 /* supports 16-bit address space */
+#define I16HEX 1 /* supports 20-bit address space */
+#define I32HEX 2 /* supports 32-bit address space */
 
-#define REC_DAT 0 
-#define REC_EOF 1 
-#define REC_ESA 2 
-#define REC_SSA 3 
-#define REC_ELA 4 
-#define REC_SLA 5
+/* ihex record types */
+#define REC_DAT 0 /* data */
+#define REC_EOF 1 /* end of file */
+#define REC_ESA 2 /* extended segment address */
+#define REC_SSA 3 /* start segment address */
+#define REC_ELA 4 /* extended linear address */
+#define REC_SLA 5 /* start linear address */
 
 static char *copyright = "vasm Intel HEX output module 0.1 (c) 2020 Rida Dzhaafar";
 
-static int ihex_mode = I8HEX;
+static int ihex_fmt = I32HEX; /* default ihex format */
 
-static uint8_t *buffer;       
-static uint8_t buffer_s = 10;  
-static uint8_t buffer_i = 0;
+static uint8_t *buffer;       /* output buffer for data records */
+static uint8_t buffer_s = 10; /* maximum buffer size */
+static uint8_t buffer_i = 0;  /* current index in buffer */
 
-static uint32_t pos = 0;
-static uint16_t last_ext = 0; /* last written extended segment/linear address */
+static uint32_t addr = 0;     /* current output address */
+static uint16_t ext_addr = 0; /* last written extended segment/linear address */
 
-static void write_record(FILE *f, uint8_t type)
-/* write an ihex record of TYPE to the file */
+static void write_eof_record(FILE *f)
 {
-  uint8_t csum = type;
+  fprintf(f, ":00000001FF");
+}
+
+static void write_extended_record(FILE *f)
+{
+  uint8_t csum;
+  uint16_t ext;
+  uint8_t type;
+
+  if (!ihex_fmt)
+    return;
+
+  if (ihex_fmt == I16HEX) {
+    ext = ext_addr << 4;
+    type = REC_ESA;
+  } else {
+    ext = ext_addr;
+    type = REC_ELA;
+  }
+
+  csum = type;
+  csum += 2;
+  csum += ext >> 8;
+  csum += ext;
+  csum = (~csum) + 1;
+
+  fprintf(f, ":020000%02X%04X%02X\n", type, ext, csum);
+}
+
+static void write_data_record(FILE *f)
+{
+  uint8_t csum;
   uint8_t i;
   uint32_t start;
   uint16_t ext;
 
-  switch (type) {
+  if (!buffer_i)
+    return;
 
-    case REC_DAT:
-      start = pos - buffer_i - 1;
-      ext = start >> 16;
-      start &= 0xFFFF;
-      
-      if (ext != last_ext) {
-        last_ext = ext;
-        if (ihex_mode == I16HEX)
-          write_record(f, REC_ESA);
-        else
-          write_record(f, REC_ELA);
-      }
+  start = addr - buffer_i;
+  ext = start >> 16;
+  start &= 0xFFFF;
 
-      fprintf(f, ":%02X%04X00", buffer_i, start);
-      csum += start;
-      csum += start >> 8;
-      csum += buffer_i;
-      for (i = 0; i < buffer_i; i++) {
-        csum += buffer[i];
-        fprintf(f, "%02X", buffer[i]);
-      }
-      csum = (~csum) + 1;
-      fprintf(f, "%02X\n", csum);
-      break;
-
-    case REC_EOF:
-      fprintf(f, ":00000001FF");
-      break;
-    
-    case REC_ESA:
-      ext = last_ext << 4;
-      csum += 2;
-      csum += ext >> 8;
-      csum += ext;
-      csum = (~csum) + 1;
-      fprintf(f, ":02000002%04X%02X\n", ext & 0xFFFF, csum);
-      break;
-
-    case REC_ELA:
-      ext = last_ext;
-      csum += 2;
-      csum += ext >> 8;
-      csum += ext;
-      csum = (~csum) + 1;
-      fprintf(f, ":02000004%04X%02X\n", ext, csum);
+  if (ext != ext_addr) {
+    ext_addr = ext;
+    write_extended_record(f);
   }
+
+  fprintf(f, ":%02X%04X00", buffer_i, start);
+  csum = start;
+  csum += start >> 8;
+  csum += buffer_i;
+  for (i = 0; i < buffer_i; i++) {
+    csum += buffer[i];
+    fprintf(f, "%02X", buffer[i]);
+  }
+  csum = (~csum) + 1;
+  fprintf(f, "%02X\n", csum);
+
+  buffer_i = 0;
 }
 
 static void buffer_data(FILE *f, uint8_t b)
-/* store byte in the buffer and flush if necessary */
 {
   buffer[buffer_i] = b;
   buffer_i++;
-  pos++;
-  
-  if (buffer_i == buffer_s) {
-    write_record(f, REC_DAT);
-    buffer_i = 0;
-  }
+  addr++;
+
+  if (buffer_i == buffer_s)
+    write_data_record(f);
 }
 
 static void align(FILE *f, section *sec, atom *a)
 {
-  uint32_t align = balign(pos, a->align);
-  int i;
+  uint32_t align = balign(addr, a->align);
+  uint32_t i;
   uint32_t len;
   uint8_t *fill;
 
-  if (align == 0)
+  if (!align)
     return;
 
   if (a->type == SPACE && a->content.sb->space == 0) {
@@ -120,7 +125,7 @@ static void align(FILE *f, section *sec, atom *a)
     buffer_data(f, 0);
     align--;
   }
-  printf("fill (%d): %04X\n",len,fill[0]);
+
   while (align >= len) {
     for(i = 0; i < len; i++) {
       buffer_data(f, fill[i]);
@@ -131,42 +136,46 @@ static void align(FILE *f, section *sec, atom *a)
   while (len--) {
     buffer_data(f, 0);
   }
-    
 }
 
 static void write_output(FILE *f, section *sec, symbol *sym)
 {
   uint32_t i, j;
-  atom *ap;
-  section *sp;
+  atom *a;
+  section *s;
 
   if (!sec)
     return;
-  
-  buffer = mymalloc(sizeof(uint8_t) * buffer_s);
 
   for (; sym; sym = sym->next)
     if (sym->type == IMPORT)
       output_error(6, sym->name); /* undefined symbol sym->name */
 
-  for (sp = sec; sp; sp = sp->next) {
-    pos = ULLTADDR(sp->org);
-    for (ap = sp->first; ap; ap = ap->next) {
-      align(f, sp, ap);
-      if (ap->type == DATA) {
-        for (i = 0; i < ap->content.db->size; i++)
-          buffer_data(f, ap->content.db->data[i]);
-      } else if (ap->type == SPACE) {
-        for (i = 0; i < ap->content.sb->space; i++)
-          for (j = 0; j < ap->content.sb->size; j++)
-            buffer_data(f, ap->content.sb->fill[j]);
+  buffer = mymalloc(sizeof(uint8_t) * buffer_s);
+
+  for (s = sec; s; s = s->next) {
+    addr = ULLTADDR(s->org);
+    for (a = s->first; a; a = a->next) {
+      /* check if atom needs to be aligned */
+      align(f, s, a);
+      /* write to output buffer if necessary */
+      if (a->type == DATA) {
+        for (i = 0; i < a->content.db->size; i++) {
+          buffer_data(f, a->content.db->data[i]);
+        }
+      } else if (a->type == SPACE) {
+        for (i = 0; i < a->content.sb->space; i++) {
+          for (j = 0; j < a->content.sb->size; j++) {
+            buffer_data(f, a->content.sb->fill[j]);
+          }
+        }
       }
     }
-    write_record(f, REC_DAT);
+    /* flush buffer before moving on to next section */
+    write_data_record(f);
   }
-
-  write_record(f, REC_EOF);
   
+  write_eof_record(f);
   myfree(buffer);
 }
 
@@ -174,20 +183,20 @@ static int parse_args(char *arg)
 {
   size_t size;
   if (!strcmp(arg, "-i8hex")) {
-    ihex_mode = I8HEX;
+    ihex_fmt = I8HEX;
     return 1;
   }
   else if (!strcmp(arg, "-i16hex")) {
-    ihex_mode = I16HEX;
+    ihex_fmt = I16HEX;
     return 1;
   }
   else if (!strcmp(arg, "-i32hex")) {
-    ihex_mode = I32HEX;
+    ihex_fmt = I32HEX;
     return 1;
   }
   else if (!strncmp(arg, "-record-size=", 13)) {
     size = atoi(arg + 13);
-    /* an IHEX record cannot be bigger than 0xff bytes in size */
+    /* an IHEX record cannot be longer than FF bytes */
     if (size < 1 || size > 255)
       return 0;
     buffer_s = size;
@@ -202,7 +211,6 @@ int init_output_ihex(char **cp, void (**wo)(FILE *, section *, symbol *), int (*
     output_error(1, cpuname); /* output module doesn't support cpuname */
     return 0;
   }
-
   *cp = copyright;
   *wo = write_output;
   *oa = parse_args;
